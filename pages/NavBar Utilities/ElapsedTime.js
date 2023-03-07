5443dd195a7f940c89eb70a83e24c6981f931e96
Anuma Thomas <105603374+tombrowngithub@users.noreply.github.com>
2023-03-07 16:55:43 +0100

    Update ElapsedTime.js

--- pages/NavBar Utilities/ElapsedTime.js
+++ pages/NavBar Utilities/ElapsedTime.js
@@ -9,19 +9,19 @@ export default function ElapsedTime() {
     }
 
     return (
-        <div className="w-[70%] h-[20%] bg-white shadow-lg m-auto fixed top-0 bottom-0 left-0 right-0">
+        <div className="w-[70%] h-[20%] bg-white shadow-lg m-auto fixed top-0 bottom-0 left-0 right-0 dark:bg-[#2d2d2d]">
             <div className="flex p-1 justify-between m-auto">
-                <h2 className="text-lg font-bold ml-8">Time Spent Reading:</h2>
+                <h2 className="text-lg font-bold ml-8 dark:text-white">Time Spent Reading:</h2>
                 <XMarkIcon onClick={myExit} className="w-6 bg-red-700"/>
             </div>
 
             <hr/>
-            <div className="flex flex-col text-center">
+            <div className="flex flex-col text-center dark:bg-[#616161]">
                 <hr/>
                 <div className="w-full flex flex-col mt-2 items-start">
-                    <h1 className="text-lg font-bold shadow-lg w-full flex justify-between px-5">Seconds:: <span className="text-green-700">{elapsedTime.seconds}</span></h1>
-                    <h1 className="text-lg font-bold shadow-lg w-full flex justify-between px-5">Minutes:: <span className="text-green-700">{elapsedTime.minutes}</span></h1>
-                    <h1 className="text-lg font-bold shadow-lg w-full flex justify-between px-5">Hours:: <span className="text-green-700">{elapsedTime.hours}</span></h1>
+                    <h1 className="text-lg font-bold shadow-lg w-full flex justify-between px-5 dark:text-white">Seconds:: <span className="text-green-700">{elapsedTime.seconds}</span></h1>
+                    <h1 className="text-lg font-bold shadow-lg w-full flex justify-between px-5 dark:text-white">Minutes:: <span className="text-green-700">{elapsedTime.minutes}</span></h1>
+                    <h1 className="text-lg font-bold shadow-lg w-full flex justify-between px-5 dark:text-white">Hours:: <span className="text-green-700">{elapsedTime.hours}</span></h1>
                 </div>
             </div>
         </div>
